bb5a7a608e1fc97d99379e1668965ae2f32e5e87
stevexisong <stevesong.wisc@gmail.com>
2014-10-07 17:05:56 -0500

    modified:   Remove_Duplicates_from_the_Sorted_Array.cpp

--- leetcode/Remove_Duplicates_from_the_Sorted_Array.cpp
+++ leetcode/Remove_Duplicates_from_the_Sorted_Array.cpp
@@ -1,5 +1,49 @@
 #include<stdio.h>
 
+class Solution3 {
+public:
+    static int removeDuplicates(int A[], int n) {
+        if(n<2)
+            return n;
+        int newPos = 1;
+        for(int i =1; i < n; i++){
+            if(A[i] != A[i-1]) A[newPos++] = A[i];
+        }
+        return newPos;
+    }
+};
+
+class Solution2 {
+public:
+    static int removeDuplicates(int A[], int n) {
+        int retSize = n;
+        if(n<=0)
+            return 0;
+        if(n==1)
+            return 1;
+
+        for(int curPos = retSize-1, dupPos = curPos; curPos > 0; curPos = dupPos){
+            dupPos = curPos-1;
+
+            for(int i = 0; i < n; i++)
+                printf("%d", A[i]);
+
+            while(A[dupPos] == A[curPos] && dupPos >= 0){
+                dupPos--;
+            }
+
+            int dis = curPos - dupPos - 1;
+
+            while(curPos < retSize && dis > 0){
+                A[curPos-dis] = A[curPos];
+                curPos++;
+            }
+            retSize -= dis;
+        }
+        return retSize;
+    }
+};
+
 class Solution {
 public:
     static int removeDuplicates(int A[], int n) {
@@ -30,12 +74,14 @@ public:
 
 
 int main(){
-    int A[] = { 0, 0, 1, 1, 1, 2, 3, 4,4,4, 5};
+    int A[] = { 0, 0, 1, 1, 1, 2, 3, 4, 4, 4, 5};
     int size = 11;
-    for(int i =0; i< size; i++)
+
+    for(int i = 0; i < size; i++)
         printf("%d", A[i]);
 
-    Solution sl;
+    printf("begin\n");
+    Solution3 sl;
     int retSize = sl.removeDuplicates(A,size);
 
     for(int i =0; i< retSize; i++)
